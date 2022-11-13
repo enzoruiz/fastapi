4b487c33d9de8d42f4e2ce829f10654713928b7f
Enzo Ruiz <enzo.ruizpelaez@crehana.com>
2022-11-13 14:11:39 -0500

    minor: remove commented code

--- app/main.py
+++ app/main.py
@@ -92,13 +92,3 @@ def update_product(
         return ProductService.update_product(db_product, product)
     else:
         raise HTTPException(status_code=401, detail="Not authorized to update products")
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name": item.name, "item_id": item_id}
